9c83a8470fa41047f5e49a5e3cb9192cfa01fcd2
Mateus Oliveira <m203656@dac.unicamp.br>
2022-11-13 12:52:36 -0300

    My first configuration lua

---
+++ init.lua
@@ -0,0 +1,24 @@
+--[[
+
+Neovim init file
+Maintainer: brainf+ck
+Website: https://github.com/brainfucksec/neovim-lua
+
+--]]
+
+-- Import Lua modules
+require('packer_init')
+require('core/options')
+require('core/autocmds')
+require('core/keymaps')
+require('core/colors')
+require('core/statusline')
+require('lsp/lspconfig')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+
+require('dap-python').setup('/home/paim/anaconda3/envs/ocr_estacio/bin/python3')
+require('telescope').load_extension('dap')
